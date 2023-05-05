f4816d57847ed90aefede997dbb77eb75027b9e1
Andreea Kamelya <kamelyadeea22@gmail.com>
2023-05-05 19:58:15 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const buttonElement = document.getElementById("button");
+
+let index = 0;
+
+const colors = [1, 2, 3];
+
+const changeColor = () => {
+  if (colors[index] === 1) {
+    buttonElement.classList.add("btn-yellow");
+  } else if (colors[index] === 2) {
+    buttonElement.classList.add("btn-green");
+  } else if (colors[index] === 3) {
+    buttonElement.classList.remove("btn-yellow");
+    buttonElement.classList.remove("btn-green");
+  }
+  index = index >= colors.length - 1 ? 0 : index + 1;
+};
+
+setInterval(changeColor, 10000);
+
+buttonElement.addEventListener("click", changeColor);
